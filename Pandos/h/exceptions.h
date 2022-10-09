8957bccb03eb254508bf899248003ac92499b3cc
Jake Heyser <97635108+heyserj@users.noreply.github.com>
2022-10-09 19:32:47 -0400

    Adding scheduler.h and exceptions.h

---
+++ Pandos/h/exceptions.h
@@ -0,0 +1,14 @@
+#ifndef EXCEPTIONS
+#define EXCEPTIONS
+
+/**************************************************************************** 
+ *
+ * The externals declaration file for the TLB-Refill exception handler module
+ * 
+ * Written by: Kollen Gruizenga and Jake Heyser
+ * 
+ ****************************************************************************/
+
+extern void uTLB_RefillHandler();
+
+#endif EXCEPTIONS
