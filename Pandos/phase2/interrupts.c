f4bbdf8a35f270f0ae07ada8eeac24edc2192350
Kollen Gruizenga <gruizengak@xavier.edu>
2022-10-27 19:45:56 -0400

    Replaced initial.c inclusion, using initial.h

--- Pandos/phase2/interrupts.c
+++ Pandos/phase2/interrupts.c
@@ -44,7 +44,7 @@
 #include "../h/scheduler.h"
 #include "../h/interrupts.h"
 #include "../h/exceptions.h"
-#include "../phase2/initial.c"
+#include "../h/initial.h"
 #include "/usr/include/umps3/umps/libumps.h"
 
 /* Function declarations */
