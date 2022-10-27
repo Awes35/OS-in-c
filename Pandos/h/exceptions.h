53d5b8eb202da2f280be1e72e131683fa7209dc4
Jake Heyser <97635108+heyserj@users.noreply.github.com>
2022-10-27 12:36:53 -0400

    Changing PCB to pcb in updateCurrPcb function

--- Pandos/h/exceptions.h
+++ Pandos/h/exceptions.h
@@ -15,6 +15,6 @@ extern void uTLB_RefillHandler ();
 extern void sysTrapH ();
 extern void tlbTrapH ();
 extern void pgmTrapH ();
-extern void updateCurrPCB(pcb_PTR proc);
+extern void updateCurrPcb(pcb_PTR proc);
 
 #endif
