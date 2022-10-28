ece8b6151f1bc7f6b348170c8adc49fd71cda918
Kollen Gruizenga <gruizengak@xavier.edu>
2022-10-28 13:08:30 -0400

    Test print statement

--- Pandos/phase2/p2test.c
+++ Pandos/phase2/p2test.c
@@ -152,7 +152,8 @@ void uTLB_RefillHandler () {
 /*                 p1 -- the root process                            */
 /*                                                                   */
 void test() {	
-	
+    print("Reached test()");
+    
 	SYSCALL(VERHOGEN, (int)&testsem, 0, 0);					/* V(testsem)   */
 
 	print("p1 v(testsem)\n");
