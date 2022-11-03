1996970cdd5f6a6b229034e0a88e7de094fa9c69
Jake Heyser <97635108+heyserj@users.noreply.github.com>
2022-11-02 22:11:45 -0400

    Adding currentProc = NULL

--- Pandos/phase2/exceptions.c
+++ Pandos/phase2/exceptions.c
@@ -253,7 +253,7 @@ void passUpOrDie(int exceptionCode){
 	else{
 		/* the Current Process' p_support_struct is NULL, so we handle it as a SYS2: the Current Process and all its progeny are terminated */
 		terminateProcess(currentProc); /* calling the termination function that "kills" the Current Process and all of its children */
-		/*currentProc = NULL;*/ /* make sure to set the Current Process pointer to NULL, in case the local proc ptr didn't update currentProc ptr */
+		currentProc = NULL;*/ /* make sure to set the Current Process pointer to NULL, in case the local proc ptr didn't update currentProc ptr */
 		switchProcess(); /* calling the Scheduler to begin executing the next process */
 	}
 }
@@ -293,7 +293,7 @@ void sysTrapH(){
 
 		case SYS2NUM: /* if the SYSCALL number is 2 */
 			terminateProcess(currentProc); /* invoking the internal function that handles SYS2 events */
-			/*currentProc = NULL;*/ /* make sure to set the Current Process pointer to NULL, in case the local proc ptr didn't update currentProc ptr */
+			currentProc = NULL;*/ /* make sure to set the Current Process pointer to NULL, in case the local proc ptr didn't update currentProc ptr */
 			switchProcess(); /* calling the Scheduler to begin executing the next process */
 		
 		case SYS3NUM: /* if the SYSCALL number is 3 */
