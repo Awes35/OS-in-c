708a326fe142b2231a060ae9ca45172299dc0e06
Kollen Gruizenga <gruizengak@xavier.edu>
2022-11-02 21:28:07 -0400

    no PLTON

--- Pandos/phase2/scheduler.c
+++ Pandos/phase2/scheduler.c
@@ -84,7 +84,7 @@ void switchProcess(){
 		if ((procCnt > 0) && (softBlockCnt > 0)){ /* if the number of started, but not yet terminated, processes is greater than zero and there's at least one such process is "blocked" */
 			/* currentProc should be NULL.. set proc0 Status */
 			/* int stat = (ALLOFF | IMON | PLTON | IECON); */
-			setSTATUS((ALLOFF | IMON | PLTON | IECON));
+			setSTATUS((ALLOFF | IMON | IECON));
 			
 			/* currentProc->p_s.s_status = ((currentProc->p_s.s_status) | IECON | IMON); */
 			/* enabling interrupts for the Current Process so we can call the WAIT() function */
