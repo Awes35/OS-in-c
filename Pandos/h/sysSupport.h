e90e60b1c9b3bd4cdb349e696f9494c20bd74683
Jake Heyser <97635108+heyserj@users.noreply.github.com>
2022-11-20 21:45:54 -0500

    Adding sysSupport.h and start of vmSupport.h

---
+++ Pandos/h/sysSupport.h
@@ -0,0 +1,18 @@
+#ifndef SYSSUPPORT
+#define SYSSUPPORT
+
+/**************************************************************************** 
+ *
+ * The externals declaration file for the module responsible for implementing
+ * phase 3's general exception handler, SYSCALL exception handler, and Program
+ * Trap exception handler
+ * 
+ * Written by: Kollen Gruizenga and Jake Heyser
+ * 
+ ****************************************************************************/
+
+#include "../h/types.h"
+
+extern void temp();
+
+#endif
---
+++ Pandos/h/vmSupport.h
@@ -0,0 +1,19 @@
+#ifndef VMSUPPORT
+#define VMSUPPORT
+
+/**************************************************************************** 
+ *
+ * The externals declaration file for the module containing the TLB exception
+ * handler, the functions for reading and writing flash devices, and the function
+ * (initSwapStructs) which initializes both the Swap Pool table and the accompanying
+ * semaphore
+ * 
+ * Written by: Kollen Gruizenga and Jake Heyser
+ * 
+ ****************************************************************************/
+
+#include "../h/types.h"
+
+extern void initSwapStructs();
+
+#endif
