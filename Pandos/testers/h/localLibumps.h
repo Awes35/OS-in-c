83c42b7ba68f120340a4774635fda2e0a0ef05b1
Jake Heyser <97635108+heyserj@users.noreply.github.com>
2022-11-26 18:23:51 -0500

    Adding testers.h files

---
+++ Pandos/testers/h/localLibumps.h
@@ -0,0 +1,13 @@
+#ifndef LOCALLIB
+#define LOCALLIB
+
+/************************** LOCALLIBUMBP.H ******************************
+*
+*  Written by Mikeyg
+*/
+
+#include "/usr/include/umps3/umps/libumps.h"
+
+/***************************************************************/
+
+#endif
