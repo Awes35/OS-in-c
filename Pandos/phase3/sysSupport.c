aa62b61cc5f56b53dede9798214a8bd47d3a2b6a
Kollen Gruizenga <gruizengak@xavier.edu>
2022-12-07 21:41:50 -0500

    sysSupport v3git add Pandos/phase3/sysSupport.c

--- Pandos/phase3/sysSupport.c
+++ Pandos/phase3/sysSupport.c
@@ -21,15 +21,11 @@
 #include "../h/sysSupport.h"
 #include "/usr/include/umps3/umps/libumps.h"
 
-
-/* This is a helper function to get the processor state found in the Current Process' 
-Support Structure for General (non-TLB) exceptions. */
-support_t getGenSupportStruct(){ 
-    support_t *curProcSupportStruct; /* a pointer to the Current Process' Support Structure */
-
-    curProcSupportStruct = SYSCALL(SYS8NUM, 0, 0, 0); /* obtaining a pointer to the Current Process' Support Structure */
-    return curProcSupportStruct;
-}
+/* function declarations */
+HIDDEN void terminateUProc();
+HIDDEN void getTOD(state_PTR savedState);
+HIDDEN void writeToPrinter(char *virtAddr, int strLength, int procASID, state_PTR savedState);
+HIDDEN void sysTrapHandler(state_PTR savedState, support_t *curProcSupportStruct);
 
 
 /* Function that handles all passed up non-TLB exceptions (i.e., all SYSCALL exceptions numbered 9 and above, as well as all Program
@@ -92,7 +88,7 @@ void writeToPrinter(char *virtAddr, int strLength, int procASID, state_PTR saved
         error if addr outside of u-proc's logical address space (KUSEG)
         error if strLength < 0
         error if strLength > 128 */
-    if ((*virtAddr < KUSEG) || (strLength < 0) || (strLength > MAXSTRLEN)){
+    if ((virtAddr < KUSEG) || (strLength < 0) || (strLength > MAXSTRLEN)){
         SYSCALL(SYS9NUM, 0, 0, 0); /* issue a SYS9 call to terminate the u-proc, as the request info is an error */
     }
 
@@ -120,53 +116,14 @@ void writeToPrinter(char *virtAddr, int strLength, int procASID, state_PTR saved
     statusCode = temp->devreg[index].d_status; /* setting the status code from the device register of process's associated printer device */
 
 	if (statusCode != READY){ /* if the write operation led to an error status */
-        savedState->s_v0 = statusCode * (-1);
+        savedState->s_v0 = statusCode * (-1); /* return negative of the status code */
 	}
-    else{
-        savedState->s_v0 = strLength;
+    else{ /* else, successful operation */
+        savedState->s_v0 = strLength; /* return length of string transmitted */
     }
 
 	mutex(FALSE, &devSemaphores[index]); /* calling the function that releases mutual exclusion over process's printer device's device register */
-}
-
-
-/* Function for SYS 12 */
-/* causes the requesting u-proc to be suspended until a line of output has been transmitted to the 
-terminal device associated with the u-proc */
-/* Once process resumes, return the number of characters actually transmitted in u-proc's v0 if write successful */
-/* if the operation ends with a status other than "Character Transmitted" (5), then return negative of device's status value */
-void writeToTerminal(char *virtAddr, int strLength, int procASID, state_PTR savedState){
-    /* terminal: interrupt line number 7 */
-    devregarea_t *temp; /* device register area that we can use to read and write the process procASID's terminal device */
-    int index; /* the index in devreg (and in devSemaphores) of the terminal device associated with process procASID */
-	int statusCode; /* the status code from the device register associated with the device that corresponds to the highest-priority interrupt */
-
-    /* pre-checks:
-        error if addr outside of u-proc's logical address space
-        error if strLength < 0
-        error if strLength > 128
-    */
-
-	/* initializing local variables, except for statusCode, which will be initialized later */
-	temp = (devregarea_t *) RAMBASEADDR; /* initialization of temp */
-    
-    index = ((TERMINT - OFFSET) * DEVPERINT) + (procASID - 1); /* determine terminal device associated with the u-proc */
-
-
-    /* load appropriate values into the TRANSM_COMMAND field -- device register field 3 */
-    /* Upon completion of the operation, an interrupt is raised, status code set in TRANSM_STATUS -- device reg field 0 */
-    
-    /* TRANSM_COMMAND has 2 parts: 
-        command itself: TRANSM_COMMAND.TRANSM-CMD
-        char to be transmitted: TRANSM_COMMAND.TRANSM-CHAR
-    */
-
-}
-
-
-/* Function for SYS 13 */
-void readFromTerminal(char *virtAddr, state_PTR savedState){
-    /* pass */
+    switchContext(savedState); /* return */
 }
 
 
@@ -191,12 +148,12 @@ void sysTrapHandler(state_PTR savedState, support_t *curProcSupportStruct){
         case SYS11NUM: /* if the sysNum indicates a SYS11 event */
             /* a1 should contain the virtual address of the first character of the string to be transmitted */
 			/* a2 should contain the length of this string */
-            writeToPrinter((char *) (savedState->s_a1), (savedState->s_a2), savedState); /* invoking the internal function that handles SYS11 events */
+            writeToPrinter((char *) (savedState->s_a1), (savedState->s_a2), procASID, savedState); /* invoking the internal function that handles SYS11 events */
         
         case SYS12NUM: /* if the sysNum indicates a SYS12 event */
             /* a1 should contain the virtual address of the first character of the string to be transmitted */
 			/* a2 should contain the length of this string */
-            writeToTerminal((char *) (savedState->s_a1), (savedState->s_a2), savedState); /* invoking the internal function that handles SYS12 events */
+            writeToTerminal((char *) (savedState->s_a1), (savedState->s_a2), procASID, savedState); /* invoking the internal function that handles SYS12 events */
         
         case SYS13NUM: /* if the sysNum indicates a SYS13 event */
             /* a1 should contain the virtual address of a string buffer where the data read should be placed */
