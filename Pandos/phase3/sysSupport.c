2ee76b35fd18c1267fdd8d8cc77b365fde495a18
Jake Heyser <97635108+heyserj@users.noreply.github.com>
2022-12-08 21:29:26 -0500

    Adding SYS12

--- Pandos/phase3/sysSupport.c
+++ Pandos/phase3/sysSupport.c
@@ -25,6 +25,7 @@
 HIDDEN void terminateUProc();
 HIDDEN void getTOD(state_PTR savedState);
 HIDDEN void writeToPrinter(char *virtAddr, int strLength, int procASID, state_PTR savedState);
+HIDDEN void writeToTerminal(char *virtAddr, int strLength, int procASID, state_PTR savedState);
 HIDDEN void sysTrapHandler(state_PTR savedState, support_t *curProcSupportStruct);
 
 
@@ -77,7 +78,7 @@ has been transmitted to the printer device associated with the user-process.
 
 Once the user-process resumes, this function returns in the process' v0 register either:
 - the number of characters actually transmitted, if the write was successful or
-- the negative of the printer device's status value (if the operation ends with a status other than "Character Transmitted"). */
+- the negative of the printer device's status value (if the operation ends with a status other than "Device Ready"). */
 void writeToPrinter(char *virtAddr, int strLength, int procASID, state_PTR savedState){
     /* printers: interrupt line 6 */
     devregarea_t *temp; /* pointer to device register area that we can use to read and write the process procASID's printer device */
@@ -101,11 +102,11 @@ void writeToPrinter(char *virtAddr, int strLength, int procASID, state_PTR saved
     int i;
     for (i = 0; i < strLength; i++){
         /* pass info to printer device */
-        temp->devreg[index].d_data0 = *(virtAddr+(i*WORDLEN)); /* pass character to print */
+        temp->devreg[index].d_data0 = *(virtAddr + (i * WORDLEN)); /* pass character to print */
 
         /* prep to write to the printer device */
         setInterrupts(FALSE); /* calling the function that disables interrupts in order to write the COMMAND field and issue the SYS 5 atomically */
-        temp->devreg[index].d_command = PRINTCHR;
+        temp->devreg[index].d_command = PRINTCHR; /* placing the command code for printing the character into the printer's command field */
         
         SYSCALL(SYS5NUM, LINE6, (procASID - 1), WRITE); /* issuing the SYS 5 call to block the I/O requesting process until the operation completes */
         setInterrupts(TRUE); /* calling the function that enables interrupts for the Status register, since the atomically-executed steps have now been completed */
@@ -126,6 +127,57 @@ void writeToPrinter(char *virtAddr, int strLength, int procASID, state_PTR saved
     switchUContext(savedState); /* return control back to the Current Process */
 }
 
+/* Function for SYS 12. This function causes the requesting user-process to be suspended until
+a complete string of characters (of length strLength, with first character at address virtAddr) 
+has been transmitted to the terminal device associated with the user-process. 
+
+Once the user-process resumes, this function returns in the process' v0 register either:
+- the number of characters actually transmitted, if the write was successful or
+- the negative of the terminal device's status value (if the operation ends with a status other than "Character Transmitted"). */
+void writeToTerminal(char *virtAddr, int strLength, int procASID, state_PTR savedState){
+    /* terminals: interrupt line 7 */
+    devregarea_t *temp; /* pointer to device register area that we can use to read and write the process procASID's terminal device */
+    int index; /* the index in devreg (and in devSemaphores) of the terminal device associated with process procASID */
+	int statusCode; /* the terminal device's status value, located within the device's devreg */
+
+    /* pre-checks: (each lead to SYS9)
+        error if addr outside of u-proc's logical address space (KUSEG)
+        error if strLength < 0
+        error if strLength > 128 */
+    if (((unsigned int) virtAddr < KUSEG) || (strLength < 0) || (strLength > MAXSTRLEN)){
+        SYSCALL(SYS9NUM, 0, 0, 0); /* issue a SYS9 call to terminate the u-proc, as the request info is an error */
+    }
+
+	/* initializing local variables, except for statusCode, which will be initialized later */
+	temp = (devregarea_t *) RAMBASEADDR; /* initialization of temp */
+    index = ((TERMINT - OFFSET) * DEVPERINT) + (procASID - 1) + DEVPERINT; /* index of terminal device associated with the u-proc; note that terminal device semaphores for writing come after those for reading */
+    
+    mutex(TRUE, (int *) (&devSemaphores[index])); /* calling the function that gains mutual exclusion over the appropriate terminal device's device register */
+	
+    int i;
+    for (i = 0; i < strLength; i++){
+        /* prep to write to the terminal device */
+        setInterrupts(FALSE); /* calling the function that disables interrupts in order to write the COMMAND field and issue the SYS 5 atomically */
+        temp->devreg[index].t_transm_command = (*(virtAddr + (i * WORDLEN))  << TERMSHIFT) | TRANSCHAR; /* placing the command code for printing the character into the terminal's command field (and the character to be printed) */
+        
+        SYSCALL(SYS5NUM, LINE7, (procASID - 1), WRITE); /* issuing the SYS 5 call to block the I/O requesting process until the operation completes */
+        setInterrupts(TRUE); /* calling the function that enables interrupts for the Status register, since the atomically-executed steps have now been completed */
+
+    }
+
+	/* read the status returned by terminal device */
+    statusCode = temp->devreg[index].t_transm_status; /* setting the status code from the device register of process's associated terminal device */
+
+	if (statusCode != CHARTRANSM){ /* if the write operation led to an error status */
+        savedState->s_v0 = statusCode * (-1); /* return negative of the status code */
+	}
+    else{ /* else, successful operation */
+        savedState->s_v0 = strLength; /* return length of string transmitted */
+    }
+
+	mutex(FALSE, (int *) (&devSemaphores[index])); /* calling the function that releases mutual exclusion over the appropriate terminal device's device register */
+    switchUContext(savedState); /* return control back to the Current Process */
+}
 
 void sysTrapHandler(state_PTR savedState, support_t *curProcSupportStruct){
     /* initializing variables that are global to this module, as well as savedState */ 
@@ -146,9 +198,15 @@ void sysTrapHandler(state_PTR savedState, support_t *curProcSupportStruct){
             getTOD(savedState); /* invoking the internal function that handles SYS10 events */
         
         case SYS11NUM: /* if the sysNum indicates a SYS11 event */
-            /* a1 should contain the virtual address of the first character of the string to be transmitted */
+           	/* a1 should contain the virtual address of the first character of the string to be transmitted */
 			/* a2 should contain the length of this string */
             writeToPrinter((char *) (savedState->s_a1), (int) (savedState->s_a2), procASID, savedState); /* invoking the internal function that handles SYS11 events */
+			
+	case SYS12NUM: /* if the sysNum indicates a SYS12 event */
+		/* a1 should contain the virtual address of the first character of the string to be transmitted */
+			/* a2 should contain the length of this string */
+		writeToTerminal((char *) (savedState->s_a1), (int) (savedState->s_a2), procASID, savedState); /* invoking the internal function that handles SYS12 events */
+		
     }
 }
 
